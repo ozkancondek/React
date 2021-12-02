f61b760a0065609279cec33fa07648d41acb872c
ozkancondek <ozkncndek@gmail.com>
2021-12-02 20:35:37 +0100

    states

--- Tuturial/Clarusway/jsx-comp/src/components/ClassComp.js
+++ Tuturial/Clarusway/jsx-comp/src/components/ClassComp.js
@@ -1,10 +1,22 @@
 import React from "react";
 class ClassComp extends React.Component {
+  state = { student: "ozkan" };
+  state = { counter: 0 };
+  increaseCounter = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  decreaseCounter = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
   render() {
     return (
       <div>
         <h1>Welcome to ReactJS-ClassComp</h1>
         <p>Age:{this.props.x}</p>
+        <p>Student:{this.state.student}</p>
+        <p>Counter:{this.state.counter}</p>
+        <button onClick={this.increaseCounter}>Increase</button>
+        <button onClick={this.decreaseCounter}>Decrease</button>
       </div>
     );
   }
--- Tuturial/Clarusway/jsx-comp/src/components/FuncComp.js
+++ Tuturial/Clarusway/jsx-comp/src/components/FuncComp.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 function FuncComp(props) {
   let str = "FuncComp";
+  const [counter, setCounter] = useState(0);
+  const [color, colorSet] = useState("blue");
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  };
+  const decreaseCounter = () => {
+    setCounter(counter - 1);
+  };
   return (
-    <>
+    <div>
       <h1>Welcome to ReactJS-{str}</h1>
       <p>Age:{props.y}</p>
       <p>Color:{props.z.toUpperCase()}</p>
-    </>
+      <p>Counter:{counter}</p>
+      <button onClick={increaseCounter}>Increase</button>
+      <button onClick={decreaseCounter}>Decrease</button>
+    </div>
   );
 }
 
