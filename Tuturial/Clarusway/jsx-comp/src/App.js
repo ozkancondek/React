112b7ec056e2019c722635bda59aadbac52936c9
ozkancondek <ozkncndek@gmail.com>
2021-12-02 18:37:53 +0100

    class component

---
+++ Tuturial/Clarusway/jsx-comp/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ClassComponent from "./components/ClassComp";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ClassComponent />
+        {/* Edit <code>src/App.js</code> and save to reload. */}
+      </header>
+    </div>
+  );
+}
+export default App;
---
+++ Tuturial/Clarusway/jsx-comp/src/components/ClassComp.js
@@ -0,0 +1,7 @@
+import React from "react";
+class ClassComp extends React.Component {
+  render() {
+    return <h1>Welcome to ReactJS</h1>;
+  }
+}
+export default ClassComp;
