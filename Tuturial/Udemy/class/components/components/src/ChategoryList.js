91c3a502686ac0e0a582318eb1c8521cb9c88ac5
ozkancondek <ozkncndek@gmail.com>
2021-12-02 17:11:36 +0100

    Component structure

---
+++ Tuturial/Udemy/class/components/components/src/ChategoryList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ChategoryList extends Component {
+  render() {
+    return (
+      <div>
+        <h3> Chategory List</h3>
+      </div>
+    );
+  }
+}
---
+++ Tuturial/Udemy/class/components/components/src/Navy.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Navy extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Navy Component</h2> {/*  start from App.js to use it */}
+      </div>
+    );
+  }
+}
