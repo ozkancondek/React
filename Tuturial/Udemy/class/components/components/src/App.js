1adb0ccbed23f31c3164baee19930d3d2553d112
ozkancondek <ozkncndek@gmail.com>
2021-12-02 17:45:43 +0100

    react strap

--- Tuturial/Udemy/class/components/components/src/App.js
+++ Tuturial/Udemy/class/components/components/src/App.js
@@ -2,15 +2,28 @@ import "./App.css";
 import ChategoryList from "./ChategoryList";
 import Navy from "./Navy";
 import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
 
 function App() {
   return (
     // jsx
     //this div here is not exactly like in html. Its jsx
     <div className="App">
-      <Navy></Navy> {/* add navy to here  */}
-      <ChategoryList></ChategoryList>
-      <ProductList></ProductList>
+      <Container>
+        <Row>
+          <Navy></Navy> {/* add navy to here  */}
+        </Row>
+        <Row>
+          <Col xs="3">
+            {" "}
+            <ChategoryList></ChategoryList>
+          </Col>
+          <Col xs="9">
+            {" "}
+            <ProductList></ProductList>
+          </Col>
+        </Row>
+      </Container>
     </div>
     // this is not allowed because it needs to contain container. <h3>Try Line</h3>
   );
--- Tuturial/Udemy/class/components/components/src/ChategoryList.js
+++ Tuturial/Udemy/class/components/components/src/ChategoryList.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class ChategoryList extends Component {
   render() {
     return (
       <div>
         <h3> Chategory List</h3>
+        <ListGroup flush>
+          <ListGroupItem disabled href="#" tag="a">
+            Cras justo odio
+          </ListGroupItem>
+          <ListGroupItem href="#" tag="a">
+            Dapibus ac facilisis in
+          </ListGroupItem>
+          <ListGroupItem href="#" tag="a">
+            Morbi leo risus
+          </ListGroupItem>
+          <ListGroupItem href="#" tag="a">
+            Porta ac consectetur ac
+          </ListGroupItem>
+          <ListGroupItem href="#" tag="a">
+            Vestibulum at eros
+          </ListGroupItem>
+        </ListGroup>
       </div>
     );
   }
