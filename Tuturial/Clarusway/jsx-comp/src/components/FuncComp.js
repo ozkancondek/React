18b5d4e1bb52fab28e657b7105a4e05e1ec63f8d
ozkancondek <ozkncndek@gmail.com>
2021-12-03 23:44:55 +0100

    class component

--- Tuturial/Clarusway/jsx-comp/src/components/FuncComp.js
+++ Tuturial/Clarusway/jsx-comp/src/components/FuncComp.js
@@ -24,3 +24,28 @@ function FuncComp(props) {
 }
 
 export default FuncComp;
+
+/* function Avatar(props) {
+  return (
+    <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
+  );
+} 
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <Avatar user={props.author} />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}*/
