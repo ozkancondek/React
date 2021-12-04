91490d08a97a21663a8ed547817a2b9af49ea065
ozkancondek <ozkncndek@gmail.com>
2021-12-04 08:01:54 +0100

    product list

--- Tuturial/Udemy/class/components/components/ProductList.js
+++ Tuturial/Udemy/class/components/components/ProductList.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Table, Button } from "reactstrap";
+import { Table,Button } from "reactstrap";
 
 export default class ProductList extends Component {
+ 
   render() {
     return (
       <div>
@@ -20,21 +21,14 @@ export default class ProductList extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.props.products.map((product) => (
+            {this.props.products.map(product => (
               <tr key={product.id}>
                 <th scope="row">{product.id}</th>
                 <td>{product.productName}</td>
                 <td>{product.unitPrice}</td>
                 <td>{product.quantityPerUnit}</td>
                 <td>{product.unitsInStock}</td>
-                <td>
-                  <Button
-                    onClick={() => this.props.addToCart(product)}
-                    color="info"
-                  >
-                    add
-                  </Button>
-                </td>
+                <td><Button onClick={()=>this.props.addToCart(product)} color="info">add</Button></td>
               </tr>
             ))}
           </tbody>
--- Tuturial/Udemy/class/components/components/src/ChategoryList.js
+++ Tuturial/Udemy/class/components/components/src/ChategoryList.js
@@ -1,60 +1,41 @@
 import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
-export default class ChategoryList extends Component {
+export default class CategoryList extends Component {
   state = {
     categories: [],
-    /*  categories: [
-      { categoryId: 1, categoryName: "Beverages" },
-      { categoryId: 2, categoryName: "Condiments" },
-    ], */
   };
+
   componentDidMount() {
-    this.getData();
+    this.getCategories();
   }
-  getData = () => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
+
+  getCategories = () => {
+    fetch("http://localhost:3000/categories")
       .then((response) => response.json())
-      .then((json) => this.setState({ categories: json }));
+      .then((data) => this.setState({ categories: data }));
   };
-  /*  showCategory = (e) => {
-    this.setState({ currentCategory: e.categoryName });
-  }; => move it to App.js */
-  /*  } */
+
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
-
         <ListGroup>
           {this.state.categories.map((category) => (
             <ListGroupItem
+              active={
+                category.categoryName === this.props.currentCategory
+                  ? true
+                  : false
+              }
               onClick={() => this.props.changeCategory(category)}
               key={category.id}
             >
-              {category.id}
+              {category.categoryName}
             </ListGroupItem>
           ))}
         </ListGroup>
-        <h2>{this.props.currentCategory}</h2>
-
-        {/* <ListGroup flush>
-          <ListGroupItem disabled href="#" tag="a">
-            Cras justo odio
-          </ListGroupItem>
-          <ListGroupItem href="#" tag="a">
-            Dapibus ac facilisis in
-          </ListGroupItem>
-          <ListGroupItem href="#" tag="a">
-            Morbi leo risus
-          </ListGroupItem>
-          <ListGroupItem href="#" tag="a">
-            Porta ac consectetur ac
-          </ListGroupItem>
-          <ListGroupItem href="#" tag="a">
-            Vestibulum at eros
-          </ListGroupItem>
-        </ListGroup> */}
+        {/* <h4>{this.props.currentCategory}</h4> */}
       </div>
     );
   }
