638b5e5945c1eb39c22e7af9236d6013d1732f51
ozkancondek <ozkncndek@gmail.com>
2021-12-07 19:48:56 +0100

    click and mouse events examples added

--- Tuturial/Clarusway/session04-react-events/src/App.js
+++ Tuturial/Clarusway/session04-react-events/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { AddEvent } from "./components/addEventClass/AddEvent";
 import AddEventClass from "./components/addEventClass/AddEventClass";
 import EventBind from "./components/eventBinding/EventBind";
+import MouseEvents from "./components/mouseEvents/MouseEvents";
 
 const alertNowProp = () => {
   alert("You clicked to button with prop method");
@@ -13,7 +14,8 @@ function App() {
       <h1>Events in React Js</h1>
       <AddEvent sendFunc={alertNowProp} />
       <AddEventClass />
-      <EventBind />
+      {/*   <EventBind /> */}
+      <MouseEvents />
     </div>
   );
 }
--- Tuturial/Clarusway/session04-react-events/src/components/mouseEvents/MouseEvents.js
+++ Tuturial/Clarusway/session04-react-events/src/components/mouseEvents/MouseEvents.js
@@ -1,30 +1,52 @@
-import './styles.css';
+import { useState } from "react";
+import "./styles.css";
 
 const MouseEvents = () => {
+  const [coordx, setCoordx] = useState();
+  const [coordy, setCoordy] = useState();
   // click event
   // doubleClick event
   // mouseMove event
+  const handleClick = (e) => {
+    console.log("event", e);
+    console.log("this is general element", e.currentTarget);
+  };
+  const handleDoubleClick = (e) => {
+    console.log("event", e);
+    console.log("item id:", e.target.id);
+  };
+  const onMouseMove = (e) => {
+    e.target.style.backgroundColor = "red";
+    /*   setCoordx(e.nativeEvent.offsetX);
+    setCoordy(e.nativeEvent.offsetY); */
+    setCoordx(e.pageX);
+    setCoordy(e.pageY);
+  };
+  const handleMouseDown = (e) => {
+    e.target.style.backgroundColor = "green";
+    e.target.draggeble = "true";
+  };
   return (
     <>
       <h2>MouseEvents</h2>
       <p>
-        <span>X</span> and Y
+        <span>{coordx}</span> and X
       </p>
       <p>
-        <span></span> and
+        <span>{coordy}</span> and Y
       </p>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <ul>
-          <li id="todo-1">
+          <li id="todo-1" onClick={handleClick}>
             todo item 1 <span>X</span>
           </li>
-          <li id="todo-2 ">
+          <li id="todo-2 " onDoubleClick={handleDoubleClick}>
             todo item 2 <span>X</span>
           </li>
-          <li>
+          <li onMouseMove={onMouseMove}>
             todo item 3 <span>X</span>
           </li>
-          <li>
+          <li onMouseDown={handleMouseDown}>
             todo item 4 <span>X</span>
           </li>
           <li>
