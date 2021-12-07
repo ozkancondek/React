c7e70d558a9ffa5c3d030a421a1ef56e1b838c70
ozkancondek <ozkncndek@gmail.com>
2021-12-07 15:38:16 +0100

    inline styling

---
+++ Tuturial/Clarusway/styling/src/components/inline/Inline.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Inline = () => {
+  return (
+    <div>
+      <h3 style={{ backgroundColor: "blue", color: "red" }}>
+        This is is inline style
+      </h3>
+    </div>
+  );
+};
