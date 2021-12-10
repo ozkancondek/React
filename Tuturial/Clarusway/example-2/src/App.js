c0d49e82a77f1a75de8cd4cee7246cffcf658998
ozkancondek <ozkncndek@gmail.com>
2021-12-10 17:01:23 +0100

    bootstrap-reactstrap

--- Tuturial/Clarusway/example-2/src/App.js
+++ Tuturial/Clarusway/example-2/src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Navi from "./Navi";
+import Navi from "./comp-1/Navi";
+
+import { Container } from "react-bootstrap";
+import Body from "./comp-1/Body";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ textAlign: "center" }}>
       <Navi />
+      <Body />
     </div>
   );
 }
