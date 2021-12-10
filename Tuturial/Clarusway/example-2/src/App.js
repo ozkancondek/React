08be960b1df4b750e9237609b7af99eeb24b3691
ozkancondek <ozkncndek@gmail.com>
2021-12-10 17:16:49 +0100

    i-class2 tamplate

--- Tuturial/Clarusway/example-2/src/App.js
+++ Tuturial/Clarusway/example-2/src/App.js
@@ -1,15 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navi from "./comp-1/Navi";
 
-import { Container } from "react-bootstrap";
 import Body from "./comp-1/Body";
 
 function App() {
   return (
     <div className="App" style={{ textAlign: "center" }}>
-      <Navi />
-      <Body />
+      {/*  <Navi />
+      <Body /> */}
     </div>
   );
 }
