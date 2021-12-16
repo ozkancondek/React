788161f5c20d41b992a666442b43e4e38ffda8c9
ozkancondek <ozkncndek@gmail.com>
2021-12-16 14:36:20 +0100

    textfield

--- Tuturial/Clarusway/session08-styling-2/src/App.js
+++ Tuturial/Clarusway/session08-styling-2/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Buttons from "./components/material-ui/buttons/Buttons";
 import StyledComponents from "./components/styled-comp/StyledComponents";
 import CheckBoxComp from "./components/material-ui/checkbox/CheckBoxComp";
+import TextFieldComp from "./components/material-ui/text-field/TextFieldComp";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           {/*   <StyledComponents /> */}
           <Buttons />
           <CheckBoxComp />
+          <TextFieldComp />
         </header>
       </div>
     </>
