2112c55e94febdcc7c6919f1ba90ff55002e4e18
ozkancondek <ozkncndek@gmail.com>
2021-12-29 16:00:09 +0100

    Recepie app last version

--- Projects/003-recipe-app/src/pages/about/About.js
+++ Projects/003-recipe-app/src/pages/about/About.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import {
+  AboutContainer,
+  HeaderContainer,
+  InfoContainer,
+  StyledImage,
+} from "./AboutStyle";
+import codingSvg from "../../assets/coding.svg";
 
 const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-        </div>
-    )
-}
+  return (
+    <AboutContainer>
+      <StyledImage src={codingSvg} />
+      <HeaderContainer>
+        <h1>
+          About Software Developer <span>BlueBerry</span>
+        </h1>
+      </HeaderContainer>
+      <InfoContainer>
+        <h2>Hi I am Blueberry</h2>
+        <h3>I am currently participating Fullstack bootcamp</h3>
+        <h2>
+          <a href="mailto:blueberry@clarusway.com">Contact with me</a>
+        </h2>
+      </InfoContainer>
+    </AboutContainer>
+  );
+};
 
-export default About
+export default About;
--- Projects/003-recipe-app/src/pages/login/Login.js
+++ Projects/003-recipe-app/src/pages/login/Login.js
@@ -1,11 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./LoginStyles";
+import mealSvg from "../../assets/meal.svg";
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
-}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    window.location.href = "/";
+  };
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={mealSvg} />
+        <Header>{"<Clarusway/>"}Recipe</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput type="text" placeholder="username" required />
+          <StyledInput type="password" placeholder="password" required />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
 
-export default Login
+export default Login;
