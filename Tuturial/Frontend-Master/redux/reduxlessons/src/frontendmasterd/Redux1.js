abb736440584b0991a1a8b004320611585048ebb
ozkancondek <ozkncndek@gmail.com>
2022-01-23 18:20:14 +0100

    redux

--- Tuturial/Frontend-Master/redux/reduxlessons/src/frontendmasterd/Redux1.js
+++ Tuturial/Frontend-Master/redux/reduxlessons/src/frontendmasterd/Redux1.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { compose } from "redux";
+import { compose, createStore } from "redux";
 
 export const Redux1 = () => {
   //compose()
@@ -11,13 +11,39 @@ export const Redux1 = () => {
   //if I want to use all these three functions together
 
   const combinedFunctionsOutput = (string) => repeater(bolder(upperer(string)));
-  console.log(combinedFunctionsOutput("hello"));
+  //console.log(combinedFunctionsOutput("hello"));
 
   //compose method
 
   const withCompose = compose(upperer, bolder, repeater);
 
-  console.log(withCompose("hello"));
+  //console.log(withCompose("hello"));
+
+  //creating store
+
+  const initialState = { value: 0 };
+  const INCREMENT = "INCREMENT";
+  const ADD = "ADD";
+
+  const incrementAction = { type: INCREMENT };
+
+  const increment = () => ({ type: INCREMENT });
+  const add = (amount) => ({ type: ADD, payload: amount });
+
+  const reducer = (state = initialState, action) => {
+    if (action.type === INCREMENT) {
+      return { value: state.value + 1 };
+    }
+    if (action.type === ADD) {
+      return { value: state.value + action.payload };
+    }
+    return state;
+  };
+  const store = createStore(reducer);
+
+  const subscriber = store.dispatch(increment());
+  console.log(store.getState());
+  // console.log(store);
 
   return <div></div>;
 };
